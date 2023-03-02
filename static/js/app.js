668fed1bf43631bdf716e9d9030bfa746980fab5
Massimo Scanga <pazzo@Massimos-MacBook-Pro.local>
2023-03-02 10:39:37 -0500

    updated index to have filter classes for my list element. Page now filters correctly

--- static/js/app.js
+++ static/js/app.js
@@ -34,11 +34,14 @@ function updateFilters() {
 
     // 4b. Save the value that was changed as a variable.
     let elementValue = changedElement.property("value");
+
     // 4c. Save the id of the filter that was changed as a variable.
     let filterId = changedElement.attr("id");
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
+    //else delete
+
     if (elementValue) {
       filters[filterId] = elementValue;
       } else {
